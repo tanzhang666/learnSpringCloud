5ee1abe2373b2d328557e204b8361c9f4aac2397
tanzhang666 <18717735973@163.com>
2021-01-30 20:32:24 +0800

    config 客户端

---
+++ cloud-demo-parent/cloud-demo-config-client3355/src/main/java/com/learn/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,20 @@
+package com.learn.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+
+    @Value("${config.info}")
+    private String configInfo;
+
+
+    @GetMapping("/config")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
