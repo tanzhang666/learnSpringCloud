dc15c1388961496a78350a3746e53716439cb6aa
tanzhang666 <18717735973@163.com>
2021-01-30 18:37:05 +0800

    gateway过滤器

---
+++ cloud-demo-parent/cloud-demo-gateway-gateway9527/src/main/java/com/learn/springcloud/filter/GatewayFilter.java
@@ -0,0 +1,35 @@
+package com.learn.springcloud.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String uname = request.getQueryParams().getFirst("uname");
+        if(uname==null){
+            log.info("非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    //等级越高 数字越小
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
